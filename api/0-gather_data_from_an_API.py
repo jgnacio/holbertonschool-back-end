b0abfb6502b1939610ae246bc82f19b1a65ebeda
jgnacio <jgnaciogomez@gmail.com>
2023-04-02 19:19:53 -0300

    fix variable name, argv index and true conditional

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,30 +11,32 @@ from json import JSONDecodeError
 import requests
 import sys
 
+user = sys.argv[1]
+
 resp = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={sys.argv[-1]}')
+    f'https://jsonplaceholder.typicode.com/todos?userId={user}')
 employee_resp = requests.get(
-    f'https://jsonplaceholder.typicode.com/users/{sys.argv[-1]}'
+    f'https://jsonplaceholder.typicode.com/users/{user}'
 )
 
 employee_list = {}
-emplyee_info = {}
+employee_info = {}
 try:
     employee_taks = resp.json()
     employee_info = employee_resp.json()
 except JSONDecodeError:
     print('Response could not be serialized')
 
-user_id = sys.argv[-1]
 completed = total_taks = 0
 
 # Get all the taks name in order and how many are complete.
 taks_name_list = ""
 for task in employee_taks:
-    if task['completed'] == True:
+    if task['completed'] is True:
         completed += 1
-        taks_name_list += "     " + f"{task['title']}\n"
+        taks_name_list += "\t " + f"{task['title']}\n"
     total_taks += 1
+
 
 employee_name = employee_info['name']
 

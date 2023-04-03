95d0aec143473815bf8b62b3b1db96e03a0cd99b
jgnacio <jgnaciogomez406@gmail.com>
2023-04-03 05:09:09 -0700

    Constant uppercase fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,17 +11,18 @@ import json
 import sys
 import urllib.request
 
-user = sys.argv[1]
-
-todos_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user)
-employee_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user)
+USER = sys.argv[1]
+COMPLETED = TOTAL_TASK = 0
+TASK_LIST = ""
+TODOS_URL = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(USER)
+EMPLOYEE_URL = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER)
 
 # Request for the taks
-with urllib.request.urlopen(todos_url) as response:
+with urllib.request.urlopen(TODOS_URL) as response:
     todos_data = response.read().decode()
 
 # Request for the employee info
-with urllib.request.urlopen(employee_url) as response:
+with urllib.request.urlopen(EMPLOYEE_URL) as response:
     employee_data = response.read().decode()
 
 employee_info = {}
@@ -31,15 +32,13 @@ try:
 except json.JSONDecodeError:
     print('Response could not be serialized')
 
-completed = total_taks = 0
 
 # Get all the taks name in order and how many are complete.
-taks_name_list = ""
 for task in employee_taks:
     if task['completed'] is True:
-        completed += 1
-        taks_name_list += "\t {}\n".format(task['title'])
-    total_taks += 1
+        COMPLETED += 1
+        TASK_LIST += "\t {}\n".format(task['title'])
+    TOTAL_TASK += 1
 
 employee_name = employee_info['name']
 
@@ -47,6 +46,11 @@ employee_name = employee_info['name']
 print_employee = """\
 Employee {} is done with tasks({}/{}):
 {}\
-""".format(employee_name, completed, total_taks, taks_name_list[:-1])
+""".format(
+    employee_name,
+    COMPLETED,
+    TOTAL_TASK,
+    TASK_LIST[:-1]
+)
 
 print(print_employee)

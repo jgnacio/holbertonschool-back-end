b469b297ba63b14738d947ce6e04e0d64593d829
jgnacio <jgnaciogomez406@gmail.com>
2023-04-17 11:17:12 -0700

    Fix dont execute when import, and print correctly

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,34 +12,34 @@ import json
 import sys
 import urllib.request
 
-USER = sys.argv[1]
-TODOS_URL = f'https://jsonplaceholder.typicode.com/todos?userId={USER}'
-EMPLOYEE_URL = f'https://jsonplaceholder.typicode.com/users/{USER}'
-TASKS_LIST = ""
+if __name__ == '__main__':
+    USER = sys.argv[1]
+    TODOS_URL = f'https://jsonplaceholder.typicode.com/todos?userId={USER}'
+    EMPLOYEE_URL = f'https://jsonplaceholder.typicode.com/users/{USER}'
+    TASKS_LIST = ""
 
-# Request for the taks
-with urllib.request.urlopen(TODOS_URL) as response:
-    todos_data = response.read().decode()
+    # Request for the taks
+    with urllib.request.urlopen(TODOS_URL) as response:
+        todos_data = response.read().decode()
 
-# Request for the employee info
-with urllib.request.urlopen(EMPLOYEE_URL) as response:
-    employee_data = response.read().decode()
+    # Request for the employee info
+    with urllib.request.urlopen(EMPLOYEE_URL) as response:
+        employee_data = response.read().decode()
 
-employee_info = {}
-try:
-    employee_tasks = json.loads(todos_data)
-    employee_info = json.loads(employee_data)
-except json.JSONDecodeError:
-    print('Response could not be serialized')
+    employee_info = {}
+    try:
+        employee_tasks = json.loads(todos_data)
+        employee_info = json.loads(employee_data)
+    except json.JSONDecodeError:
+        print('Response could not be serialized')
 
-
-# Get all the taks name in order and how many are complete.
-for task in employee_tasks:
-    TASKS_LIST += f"\
+    # Get all the taks name in order and how many are complete.
+    for task in employee_tasks:
+        TASKS_LIST += f"\
 \"{task['userId']}\",\
 \"{employee_info['name']}\",\
 \"{task['completed']}\",\
 \"{task['title']}\"\n"
 
-with open(f'{USER}.csv', 'w', encoding='utf-8') as f:
-    f.write(TASKS_LIST[:-1])
+    with open(f'{USER}.csv', 'w', encoding='utf-8') as f:
+        f.write(TASKS_LIST[:-1])
